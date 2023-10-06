bfb9eec9b97ce45b4d4a7449e42fbe852ab70ed6
Nadiia Tsytsylina <miniova95@gmail.com>
2023-10-06 22:16:12 +0300

    update pages

--- src/pages/Catalog.js
+++ src/pages/Catalog.js
@@ -1,19 +1,17 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { fetchAdverts } from "redux/operations";
 
 import AdvertList from "components/AdvertList/AdvertList";
+import { selectAdverts } from "redux/selectors";
 
 export default function Catalog() {
   const dispatch = useDispatch();
+  const adverts = useSelector(selectAdverts);
 
   useEffect(() => {
     dispatch(fetchAdverts());
   }, [dispatch]);
 
-  return (
-    <div>
-      <AdvertList />
-    </div>
-  );
+  return <>{adverts.length > 0 && <AdvertList adverts={adverts} />}</>;
 }
--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,3 +1,18 @@
+import { useSelector } from "react-redux";
+import { selectFavorites } from "redux/selectors";
+import AdvertList from "components/AdvertList/AdvertList";
+import EmptyFavorites from "components/EmptyFavorites/EmptyFavorites";
+
 export default function Favorites() {
-  return <div>Favorites</div>;
+  const favoriteAdverts = useSelector(selectFavorites);
+
+  return (
+    <>
+      {favoriteAdverts.length > 0 ? (
+        <AdvertList adverts={favoriteAdverts} />
+      ) : (
+        <EmptyFavorites />
+      )}
+    </>
+  );
 }
