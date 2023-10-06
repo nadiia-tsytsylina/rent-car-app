f5ebd5bc332f5cec4125ca01eed2b340b8043337
Nadiia Tsytsylina <miniova95@gmail.com>
2023-10-06 22:15:16 +0300

    add Empty component for Favorites

---
+++ src/components/EmptyFavorites/EmptyFavorites.js
@@ -0,0 +1,9 @@
+import { Text } from "./EmptyFavorites.styled";
+
+export default function EmptyFavorites() {
+  return (
+    <>
+      <Text>You don't have any favorites yet</Text>
+    </>
+  );
+}
---
+++ src/components/EmptyFavorites/EmptyFavorites.styled.jsx
@@ -0,0 +1,9 @@
+import styled from "@emotion/styled";
+
+export const Text = styled.h2`
+  color: var(--dark-text);
+  font-size: 18px;
+  line-height: 1.5;
+  font-weight: 500;
+  text-align: center;
+`;
