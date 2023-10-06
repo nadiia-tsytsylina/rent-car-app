a9a0f115efb568b3d8008ea1a129ae78bde96625
Nadiia Tsytsylina <miniova95@gmail.com>
2023-10-06 19:14:55 +0300

    add routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,18 @@
-export const App = () => {
+import { Routes, Route } from "react-router-dom";
+
+import Home from "pages/Home";
+import Catalog from "pages/Catalog";
+import Favorites from "pages/Favorites";
+import Layout from "./Layout/Layout";
+
+export default function App() {
   return (
-    <div
-      style={{
-        height: "100vh",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        fontSize: 40,
-        color: "#010101",
-      }}
-    >
-      React homework template
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="catalog" element={<Catalog />} />
+        <Route path="favorites" element={<Favorites />} />
+      </Route>
+    </Routes>
   );
-};
+}
