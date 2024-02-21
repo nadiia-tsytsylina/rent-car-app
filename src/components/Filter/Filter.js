cc40a1ef28d252d4c40b7bc55927dbad4916e6f4
Nadiia Tsytsylina <miniova95@gmail.com>
2024-02-21 13:25:50 +0200

    updated Filter

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -1,10 +1,9 @@
-import { nanoid } from "@reduxjs/toolkit";
 import { Formik } from "formik";
 import { toast } from "react-toastify";
 import { useSelector, useDispatch } from "react-redux";
-import { selectAllAdverts } from "redux/selectors";
-import { updateFilter } from "redux/filterSlice";
-
+import { selectAllAdverts, selectIsFiltered } from "redux/selectors";
+import { resetFilter, updateFilter } from "redux/filterSlice";
+import { MIN_MILEAGE, MAX_MILEAGE, MAX_PRICE } from "constants/constants";
 import {
   FilterContainer,
   StyledForm,
@@ -19,15 +18,21 @@ import {
 export default function Filter() {
   const dispatch = useDispatch();
   const allAdverts = useSelector(selectAllAdverts);
+  const isFiltered = useSelector(selectIsFiltered);
+
   const allBrands = new Array(
     ...allAdverts
       .map((item) => item.make)
       .filter((make, index, array) => array.indexOf(make) === index)
+      .sort((a, b) => a.localeCompare(b))
   );
-  const priceList = [
-    30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180,
-    190, 200,
-  ];
+
+  const allPrices = new Array(
+    ...allAdverts
+      .map((item) => Number(parseFloat(item.rentalPrice.replace("$", ""))))
+      .filter((price, index, array) => array.indexOf(price) === index)
+      .sort((a, b) => a - b)
+  );
 
   const handleSubmit = ({ brand, price, minMileage, maxMileage }, actions) => {
     if (minMileage && maxMileage && minMileage >= maxMileage) {
@@ -37,80 +42,97 @@ export default function Filter() {
     dispatch(updateFilter({ brand, price, minMileage, maxMileage }));
   };
 
+  const clearFilters = () => {
+    dispatch(resetFilter());
+  };
+
   return (
     <FilterContainer>
       <Formik
         initialValues={{
           brand: "",
-          price: "",
-          minMileage: "",
-          maxMileage: "",
+          price: MAX_PRICE,
+          minMileage: MIN_MILEAGE,
+          maxMileage: MAX_MILEAGE,
         }}
         onSubmit={handleSubmit}
       >
-        <StyledForm>
-          <SelectContainer>
-            <Label htmlFor="brand">Car brand</Label>
-            <StyledSelect
-              as="select"
-              name="brand"
-              id="brand"
-              placeholder="Select a car brand"
-              required
-            >
-              <StyledOption disabled hidden value=""></StyledOption>
-              {allBrands.map((item) => (
-                <StyledOption key={nanoid()} value={item}>
-                  {item}
-                </StyledOption>
-              ))}
-            </StyledSelect>
-          </SelectContainer>
-          <SelectContainer>
-            <Label htmlFor="price">Price/ 1 hour</Label>
-            <StyledSelect
-              as="select"
-              name="price"
-              id="price"
-              placeholder="To a $"
-              required
-            >
-              <StyledOption disabled hidden value=""></StyledOption>
-              {priceList.map((item) => (
-                <StyledOption key={nanoid()} value={item}>
-                  {item}
-                </StyledOption>
-              ))}
-            </StyledSelect>
-          </SelectContainer>
+        {({ resetForm }) => (
+          <StyledForm>
+            <SelectContainer>
+              <Label htmlFor="brand">Car brand</Label>
+              <StyledSelect
+                as="select"
+                name="brand"
+                id="brand"
+                placeholder="Select a car brand"
+                required
+              >
+                <StyledOption disabled hidden value=""></StyledOption>
+                {allBrands.map((item) => (
+                  <StyledOption key={item} value={item}>
+                    {item}
+                  </StyledOption>
+                ))}
+              </StyledSelect>
+            </SelectContainer>
+            <SelectContainer>
+              <Label htmlFor="price">Max price/ 1 hour</Label>
+              <StyledSelect
+                as="select"
+                name="price"
+                id="price"
+                placeholder="To a $"
+                required
+              >
+                <StyledOption disabled hidden value=""></StyledOption>
+                {allPrices.map((item) => (
+                  <StyledOption key={item} value={item}>
+                    {item}
+                  </StyledOption>
+                ))}
+              </StyledSelect>
+            </SelectContainer>
 
-          <SelectContainer>
-            <Label>Сar mileage / km</Label>
-            <div>
-              <StyledInput
-                name="minMileage"
-                placeholder="From"
-                type="number"
-                min={1000}
-                max={10000}
-                required
-              />
-              <StyledInput
-                name="maxMileage"
-                placeholder="To"
-                type="number"
-                min={1000}
-                max={10000}
-                required
-                style={{
-                  borderRadius: "0px 14px 14px 0px",
-                  borderRight: "none",
+            <SelectContainer>
+              <Label>Сar mileage / km</Label>
+              <div>
+                <StyledInput
+                  name="minMileage"
+                  placeholder="From"
+                  type="number"
+                  min={MIN_MILEAGE}
+                  max={MAX_MILEAGE}
+                  required
+                />
+                <StyledInput
+                  name="maxMileage"
+                  placeholder="To"
+                  type="number"
+                  min={MIN_MILEAGE}
+                  max={MAX_MILEAGE}
+                  required
+                  style={{
+                    borderRadius: "0px 14px 14px 0px",
+                    borderRight: "none",
+                  }}
+                />
+              </div>
+            </SelectContainer>
+            <SearchButton type="submit">Search</SearchButton>
+            {isFiltered && (
+              <SearchButton
+                type="button"
+                onClick={() => {
+                  clearFilters();
+                  resetForm();
                 }}
-              />
-            </div>
-          </SelectContainer>
-          <SearchButton type="submit">Search</SearchButton>
-        </StyledForm>
+              >
+                Clear filters
+              </SearchButton>
+            )}
+          </StyledForm>
+        )}
       </Formik>
     </FilterContainer>
   );
